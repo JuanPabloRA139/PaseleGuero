63be3534463d87d92459a342b5d5f34c72a37f8e
dahiann04 <20243ds121@utez.edu.mx>
2025-12-11 23:29:32 -0600

    agregué MainActivity con configuración base de navegación

--- PaseleGuero/app/src/main/java/com/utez/paseleguero/MainActivity.kt
+++ PaseleGuero/app/src/main/java/com/utez/paseleguero/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            
             val navController = rememberNavController()
             Surface(color = MaterialTheme.colorScheme.background) {
                 NavGraph(navController = navController)
